6b201ef70dd1bc6c6a9dbd44dd19c17bc6fc181d
abc-yangcheng <2331471230@qq.com>
2021-01-15 18:05:32 +0800

    修改图标

---
+++ app/src/main/java/com/yangchengweather/android/logic/network/WeatherService.kt
@@ -0,0 +1,18 @@
+package com.yangchengweather.android.logic.network
+
+import com.yangchengweather.android.SunnyWeatherApplication
+import com.yangchengweather.android.logic.model.DailyResponse
+import com.yangchengweather.android.logic.model.RealtimeResponse
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface WeatherService {
+
+    @GET("v2.5/${SunnyWeatherApplication.TOKEN}/{lng},{lat}/realtime.json")
+    fun getRealtimeWeather(@Path("lng") lng: String, @Path("lat") lat: String): Call<RealtimeResponse>
+
+    @GET("v2.5/${SunnyWeatherApplication.TOKEN}/{lng},{lat}/daily.json")
+    fun getDailyWeather(@Path("lng") lng: String, @Path("lat") lat: String): Call<DailyResponse>
+
+}
--- app/src/main/java/com/yangchengweather/android/ui/place/PlaceFragment.kt
+++ app/src/main/java/com/yangchengweather/android/ui/place/PlaceFragment.kt
@@ -1,5 +1,6 @@
 package com.yangchengweather.android.ui.place
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,7 +10,9 @@ import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.*
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.yangchengweather.android.MainActivity
 import com.yangchengweather.android.R
+import com.yangchengweather.android.ui.weather.WeatherActivity
 import kotlinx.android.synthetic.main.fragment_place.*
 
 class PlaceFragment : Fragment() {
@@ -19,37 +22,47 @@ class PlaceFragment : Fragment() {
     private lateinit var adapter: PlaceAdapter
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_place,container,false)
+        return inflater.inflate(R.layout.fragment_place, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        if (activity is MainActivity && viewModel.isPlaceSaved()) {
+            val place = viewModel.getSavedPlace()
+            val intent = Intent(context, WeatherActivity::class.java).apply {
+                putExtra("location_lng", place.location.lng)
+                putExtra("location_lat", place.location.lat)
+                putExtra("place_name", place.name)
+            }
+            startActivity(intent)
+            activity?.finish()
+            return
+        }
         val layoutManager = LinearLayoutManager(activity)
         recyclerView.layoutManager = layoutManager
-        adapter = PlaceAdapter(this,viewModel.placeList)
+        adapter = PlaceAdapter(this, viewModel.placeList)
         recyclerView.adapter = adapter
-        searchPlaceEdit.addTextChangedListener{editable ->
+        searchPlaceEdit.addTextChangedListener { editable ->
             val content = editable.toString()
-            if (content.isNotEmpty()){
+            if (content.isNotEmpty()) {
                 viewModel.searchPlaces(content)
-            }else{
+            } else {
                 recyclerView.visibility = View.GONE
                 bgImageView.visibility = View.VISIBLE
                 viewModel.placeList.clear()
                 adapter.notifyDataSetChanged()
             }
         }
-
-        viewModel.placeLiveData.observe(viewLifecycleOwner, Observer { result ->
+        viewModel.placeLiveData.observe(viewLifecycleOwner, Observer{ result ->
             val places = result.getOrNull()
-            if (places != null){
+            if (places != null) {
                 recyclerView.visibility = View.VISIBLE
-                bgImageView.visibility =View.GONE
+                bgImageView.visibility = View.GONE
                 viewModel.placeList.clear()
                 viewModel.placeList.addAll(places)
                 adapter.notifyDataSetChanged()
-            }else{
-                Toast.makeText(activity,"未能查询到任何地点",Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(activity, "未能查询到任何地点", Toast.LENGTH_SHORT).show()
                 result.exceptionOrNull()?.printStackTrace()
             }
         })
